6eee12b7d2637545219ff5d50c4995764abd0c91
Abey Onalaja <abey.onalaja@gmail.com>
2015-11-18 23:48:10 +0000

    Add css

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -1,6 +1,51 @@
 module SeatSaver where
 
-import Html
+import Html exposing (..) 
+import Html.Attributes exposing (..)
 
+main: Html
 main =
-  Html.text "Hello from elm you bad boy"
+  view init
+
+
+-- MODEL
+
+
+type alias Seat =
+  {
+    seatNo : Int
+  , occupied : Bool
+  }
+
+
+
+type alias Model =
+  List Seat
+
+
+init : Model
+init =
+  [ { seatNo = 1, occupied = False }
+  , { seatNo = 2, occupied = False }
+  , { seatNo = 3, occupied = False }
+  , { seatNo = 4, occupied = False }
+  , { seatNo = 5, occupied = False }
+  , { seatNo = 6, occupied = False }
+  , { seatNo = 7, occupied = False }
+  , { seatNo = 8, occupied = False }
+  , { seatNo = 9, occupied = False }
+  , { seatNo = 10, occupied = False }
+  , { seatNo = 11, occupied = False }
+  , { seatNo = 12, occupied = False }
+  ]
+
+
+-- VIEW
+
+view : Model -> Html
+view model =
+  ul [ class "seats" ] (List.map seatItem model )
+
+
+seatItem seat =
+  li [ class "seat available"] [ text (toString seat.seatNo) ]
